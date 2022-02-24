fb2d2ee94d88e2d39c2888642bbdac9c9bb096cd
Killers0992 <Killers0992@gmail.com>
2022-02-24 23:15:27 +0100

    BOOls

--- TwitchVrcAvatarOSC/OscActions.cs
+++ TwitchVrcAvatarOSC/OscActions.cs
@@ -57,7 +57,7 @@ namespace TwitchVrcAvatarOSC
             else if (obj is Color32 color)
                 _client?.Send(action, color);
             else if (obj is bool b)
-                _client?.Send(action, b ? 1 : 0);
+                _client?.Send(action, b);
         }
 
         public static void TryExecuting()
